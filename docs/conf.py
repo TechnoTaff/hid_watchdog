cdf1a15a8179687cdb0d54e2d13c2fd62eec8d17
Steve Randall <TechnoTaff@users.noreply.github.com>
2025-06-17 22:07:30 +0100

    Revert "Refactor: Complete project overhaul and modernization"

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,7 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('..'))
 
 import hid_watchdog
 
@@ -33,24 +32,24 @@ import hid_watchdog
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = "index"
+master_doc = 'index'
 
 # General information about the project.
-project = "hid-watchdog"
-copyright = "2019, Steve Randall"
-author = "Steve Randall"
+project = u'hid-watchdog'
+copyright = u"2019, Steve Randall"
+author = u"Steve Randall"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
@@ -71,10 +70,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -85,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -96,13 +95,13 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "hid_watchdogdoc"
+htmlhelp_basename = 'hid_watchdogdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -111,12 +110,15 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
+
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
+
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
+
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -126,13 +128,9 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (
-        master_doc,
-        "hid_watchdog.tex",
-        "hid-watchdog Documentation",
-        "Steve Randall",
-        "manual",
-    ),
+    (master_doc, 'hid_watchdog.tex',
+     u'hid-watchdog Documentation',
+     u'Steve Randall', 'manual'),
 ]
 
 
@@ -140,7 +138,11 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "hid_watchdog", "hid-watchdog Documentation", [author], 1)]
+man_pages = [
+    (master_doc, 'hid_watchdog',
+     u'hid-watchdog Documentation',
+     [author], 1)
+]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -149,13 +151,13 @@ man_pages = [(master_doc, "hid_watchdog", "hid-watchdog Documentation", [author]
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (
-        master_doc,
-        "hid_watchdog",
-        "hid-watchdog Documentation",
-        author,
-        "hid_watchdog",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
+    (master_doc, 'hid_watchdog',
+     u'hid-watchdog Documentation',
+     author,
+     'hid_watchdog',
+     'One line description of project.',
+     'Miscellaneous'),
 ]
+
+
+
